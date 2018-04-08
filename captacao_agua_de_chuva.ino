20cf879cff285c21bea32e0126702c377c689d55
filipemmac <filipemmac@gmail.com>
2018-04-08 15:24:35 -0300

    Add files via upload

---
+++ captacao_agua_de_chuva.ino
@@ -0,0 +1,47 @@
+//Autor: FILIPEFLOP
+ 
+//Carrega a biblioteca do sensor ultrassonico
+#include <Ultrasonic.h>
+ 
+//Define os pinos para o trigger e echo
+#define pino_trigger 4
+#define pino_echo 5
+
+//Porta ligada ao pino IN1 do modulo
+int bomba_liga = 7;
+ 
+//Inicializa o sensor nos pinos definidos acima
+Ultrasonic ultrasonic(pino_trigger, pino_echo);
+ 
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Lendo dados do sensor...");
+}
+ 
+void loop()
+{
+  //Le as informacoes do sensor, em cm e pol
+  float cmMsec, inMsec;
+  long microsec = ultrasonic.timing();
+  cmMsec = ultrasonic.convert(microsec, Ultrasonic::CM);
+  inMsec = ultrasonic.convert(microsec, Ultrasonic::IN);
+  //Exibe informacoes no serial monitor
+  Serial.print("Distancia em cm: ");
+  Serial.print(cmMsec);
+  Serial.print(" - Distancia em polegadas: ");
+  Serial.println(inMsec);
+  delay(1000);
+
+  {
+  //Define pinos para o rele como saida
+  pinMode(bomba_liga, OUTPUT); 
+ 
+}
+{
+  digitalWrite(bomba_liga, LOW);  //Liga rele 1
+  delay(2000);
+  digitalWrite(bomba_liga, HIGH); //Desliga rele 1
+  delay(2000);
+}
+}
